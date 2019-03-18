1a08e535cdc6985270b6a8b364969e0d962be23f
Alex Carruthers <alex.carruthers0@gmail.com>
2019-03-18 18:03:57 +0000

    stocks
    added fade
    remove columns

--- data1.php
+++ data1.php
@@ -49,41 +49,43 @@
 
         <div class="row">
             <!-- main column content -->
-            <div class="col-sm-8">
+            <div class="col-sm-12" id="main">
                 <h1>Stocks</h1>
-                <div class="chart-container" style="max-width:8000px; max-height:600px">
-                    <canvas id="myChart" width="800" height="600"></canvas>
+                <div id="search">
+                    <h4>Search</h4>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" id="stockname" onkeypress="clickEnter(event)"
+                            placeholder="Enter company name">
+                        <button class="btn btn-info my-2 my-sm-0" type="button" onclick="getPossibleStocks()">Get Stock
+                            Data</button>
+                    </form>
+                    <br>
+                    <p>Enter your desired company in the input field above and click the "Get Stock Data" button to see
+                        the
+                        companies stocks represented in the chart.</p>
+                </div>
+                <div id="refine">
+                    <h4>Refine</h4>
+                    <table id="myTable">
+                        <thead>
+                            <tr>
+                                <th>Symbol</th>
+                                <th>Company Name</th>
+                                <th>Type</th>
+                                <th>Region</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <!-- end main column content-->
-
-            <!-- sidebar column content-->
-            <div class="col-sm-4" id="sidebar">
-                <h4>Stock Data</h4>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" id="stockname" onkeypress="clickEnter(event)"
-                        placeholder="Enter company name">
-                    <button class="btn btn-info my-2 my-sm-0" type="button" onclick="getPossibleStocks()">Get Stock
-                        Data</button>
-                </form>
-                <br>
-                <p>Enter your desired company in the input field above and click the "Get Stock Data" button to see the
-                    companies stocks represented in the chart.</p>
-                <table id="myTable">
-                    <thead>
-                        <tr>
-                            <th>Symbol</th>
-                            <th>Company Name</th>
-                            <th>Type</th>
-                            <th>Region</th>                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
+            <div class="chart-container"  max-height:600px" id="view">
+                <h4>View</h4>
+                <canvas id="myChart" height="600"></canvas>
             </div>
-            <!-- end sidebar column content -->
         </div>
+        <!-- end main column content-->
 
         <?php include 'includes/footer.php' ?>
     </div>
@@ -114,33 +116,35 @@
             });
 
         }
-        
+
         function getPossibleStocks() {
+            $("#search").fadeOut();
             let searchTerm = document.getElementById("stockname").value;
             let query_url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + searchTerm + "&apikey=" + apikey;
-            $.getJSON(query_url, function (json) {                
+            $.getJSON(query_url, function (json) {
                 $('#myTable tbody').empty(); //clear table but leave headers
                 let symbol, company_name, type, region; //table headers              
-                for (let i = 0; i < json.bestMatches.length; i++) {                 
+                for (let i = 0; i < json.bestMatches.length; i++) {
                     symbol = json.bestMatches[i]["1. symbol"];
                     company_name = json.bestMatches[i]["2. name"];
                     type = json.bestMatches[i]["3. type"];
                     region = json.bestMatches[i]["4. region"];
-                    $('#myTable > tbody:last-child').append('<tr><td><button class="btn btn-info my-2 my-sm-0" id="' + symbol + '"type="button" onclick="getData(this.id)">' + symbol +'</button ></td> <td>' + company_name + '</td><td>' + type + '</td><td>' + region + '</td></tr>'); //add new row to table
+                    $('#myTable > tbody:last-child').append('<tr><td><button class="btn btn-info my-2 my-sm-0" id="' + symbol + '"type="button" onclick="getData(this.id)">' + symbol + '</button ></td> <td>' + company_name + '</td><td>' + type + '</td><td>' + region + '</td></tr>'); //add new row to table
+                    $("#refine").fadeIn();
                 }
             });
         }
 
         function getData(clicked_symbol) {
-            var query_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol=" + clicked_symbol +"&apikey=" + apikey +"&datatype=json";
-           
-                $("#sidebar").fadeOut();
-           
+            $("#refine").fadeOut();
+            var query_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol=" + clicked_symbol + "&apikey=" + apikey + "&datatype=json";
             $.getJSON(query_url, function (json) {
-                console.log(json);
+                console.log(json); 
+                $("#view").fadeIn();
                 Gjson = json;
                 createDataPoints();
                 drawChart();
+
             });
         }
 
@@ -170,6 +174,7 @@
 
 
         function drawChart() {
+          
             // OHLC
             var ctx1 = document.getElementById("myChart").getContext("2d");
             ctx1.canvas.width = 1000;
@@ -191,6 +196,9 @@
                 },
             });
         }
+
+        $("#refine").hide();
+        $("#view").hide();
     </script>
 </body>
 
