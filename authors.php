5b22612a3b51346997c0846211a7eca45e520f32
andmca <mcavoy1995@gmail.com>
2019-04-02 20:10:52 +0100

    Fixed bug where multiple author profiles were showing

--- authors.php
+++ authors.php
@@ -9,7 +9,8 @@
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- jquery,popper,bootstrap -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -47,30 +48,38 @@
             <div class="col-sm-3">
                 <h1>Authors</h1>
                 <small class="text-muted">Click each team member's name to view his profile.<br>
-                    <i>When an author is selected their respective button will be highlighted grey; authors will display on the page in alphabetical order.</i></small>
+                    <i>When an author is selected their respective button will be highlighted grey; authors will display
+                        on the page in alphabetical order.</i></small>
                 <div class="btn-group-vertical btn-block">
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#afaqInfo" aria-expanded="false" aria-controls="afaqInfo">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#afaqInfo"
+                        aria-expanded="false" aria-controls="afaqInfo">
                         Afaq
                     </button>
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#alexInfo" aria-expanded="false" aria-controls="alexInfo">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#alexInfo"
+                        aria-expanded="false" aria-controls="alexInfo">
                         Alex
                     </button>
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#andrewInfo" aria-expanded="false" aria-controls="andrewInfo">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#andrewInfo"
+                        aria-expanded="false" aria-controls="andrewInfo">
                         Andrew
                     </button>
 
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#davidInfo" aria-expanded="false" aria-controls="davidInfo">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#davidInfo"
+                        aria-expanded="false" aria-controls="davidInfo">
                         David
                     </button>
 
 
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#jackInfo" aria-expanded="false" aria-controls="jackInfo">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#jackInfo"
+                        aria-expanded="false" aria-controls="jackInfo">
                         Jack
                     </button>
-                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#mattInfo" aria-expanded="false" aria-controls="mattInfo">
+                    <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#mattInfo"
+                        aria-expanded="false" aria-controls="mattInfo">
                         Matt
                     </button>
-                    <button class="btn btn-success" type="button" data-toggle="collapse" data-target="#" aria-expanded="false" aria-controls="#" onclick="clearBtns()">
+                    <button class="btn btn-success" type="button" data-toggle="collapse" data-target="#"
+                        aria-expanded="false" aria-controls="#" onclick="clearBtns()">
                         Close all buttons
                     </button>
                 </div>
@@ -81,12 +90,18 @@
             <div class="col-sm-9">
                 <div class="collapse" id="afaqInfo">
                     <div class="card card-body">
-                        <h2>Andrew McAvoy <img src="authorimg/andrew.png" class="author-img-profile" alt=""></h2>
+                        <h2>Afaq Ahmad <img src="authorimg/afaq.png" class="author-img-profile" alt=""></h2>
                         <br>
                         <h6 class="card-subtitle mb-2 text-muted">S1625481 | AAHMAD204@caledonian.ac.uk</h6>
                         <p>Project roles: <b>Software Architect</b> & <b>Process Analyst</b></p>
-                        <p>My roles in this project will be to play my part as a software architect and process analyst. As a software architect I was responsible for making the high level designs choices and dictate technical standards. I was responsible for deciding on the software pattern that would best suit Locate-A-Quake. The pattern decided was event based and MVC for the user to manipulate the view without changing the underlying data. </p>
-                        <p>As a process analyst I worked with my team members to decide on the best way to undertake the project and break down the work into smaller tasks as this will help everyone improve their understanding of the project.</p>
+                        <p>My roles in this project will be to play my part as a software architect and process analyst.
+                            As a software architect I was responsible for making the high level designs choices and
+                            dictate technical standards. I was responsible for deciding on the software pattern that
+                            would best suit Locate-A-Quake. The pattern decided was event based and MVC for the user to
+                            manipulate the view without changing the underlying data. </p>
+                        <p>As a process analyst I worked with my team members to decide on the best way to undertake the
+                            project and break down the work into smaller tasks as this will help everyone improve their
+                            understanding of the project.</p>
                         <br>
                         <span style="font-size: 2em;">
                             <i class="fab fa-linkedin-in"></i>
@@ -94,7 +109,6 @@
                         </span>
                     </div>
                 </div>
-
 
                 <div class="collapse" id="alexInfo">
                     <div class="card card-body">
@@ -109,8 +123,10 @@
                         </p>
                         <br>
                         <span style="font-size: 2em;">
-                            <a href="https://www.linkedin.com/in/alexander-carruthers-2018/" target="blank"><i class="fab fa-linkedin-in"></i></a>
-                            <a href="https://github.com/alexandercarruthers" target="blank"><i class="fab fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/alexander-carruthers-2018/" target="blank"><i
+                                    class="fab fa-linkedin-in"></i></a>
+                            <a href="https://github.com/alexandercarruthers" target="blank"><i
+                                    class="fab fa-github"></i></a>
                         </span>
                     </div>
                 </div>
@@ -122,15 +138,16 @@
                         <h6 class="card-subtitle mb-2 text-muted">S1316078 | AMCAVO201@caledonian.ac.uk</h6>
                         <p>Project roles: <b>Frontend Developer</b> & <b>Tester</b></p>
                         <p>As a front-end developer I am responsible for implementing the visual elements that users see
-                            when they interact with the web application. This involves coding the CSS stylesheets and
-                            utilising frameworks. Also, there is some JavaScript scripting involved, to manipulate some
+                            when they interact with the web application. This involves utilising CSS stylesheets and
+                            frameworks. Also, there is some JavaScript scripting involved, to manipulate some
                             of the elements on the webpage.</p>
                         <p>As tester, it is my role to design testing scenarios for usability, acceptance, and unit
                             tests. I also interact with all developers throughout the implementation stage to ensure all
                             deliverables are fit for purpose and meet the project specifications.</p>
                         <br>
                         <span style="font-size: 2em;">
-                            <a href="https://linkedin.com/in/andmca" target="blank"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="https://linkedin.com/in/andmca" target="blank"><i
+                                    class="fab fa-linkedin-in"></i></a>
                             <a href="https://github.com/andmca" target="blank"><i class="fab fa-github"></i></a>
                         </span>
                     </div>
@@ -155,7 +172,8 @@
                             above. </p>
                         <br>
                         <span style="font-size: 2em;">
-                            <a href="http://linkedin.com/in/iamfagan" target="blank"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="http://linkedin.com/in/iamfagan" target="blank"><i
+                                    class="fab fa-linkedin-in"></i></a>
                             <a href="https://github.com/fagand" target="blank"><i class="fab fa-github"></i></a>
                         </span>
                     </div>
@@ -167,7 +185,13 @@
                         <br>
                         <h6 class="card-subtitle mb-2 text-muted">S1628300 | JMOORE200@caledonian.ac.uk</h6>
                         <p>Project roles: <b>SCRUM Master</b> & <b>Software Tester</b></p>
-                        <p>As a Scrum Master I was required to ensure the team followed an agile methodology when implementing new features to the site and to regularly update the next goal in the development of the locate-a-quake site. By using Scrum we aimed to regularly update the site with new functionality. As a software tester my role involved navigating the site and evaluating the user experience and looking for any errors or broken features that need to be repaired. This role exists to ensure that the site is working properly and gives users the experience we intended.</p>
+                        <p>As a Scrum Master I was required to ensure the team followed an agile methodology when
+                            implementing new features to the site and to regularly update the next goal in the
+                            development of the locate-a-quake site. By using Scrum we aimed to regularly update the site
+                            with new functionality. As a software tester my role involved navigating the site and
+                            evaluating the user experience and looking for any errors or broken features that need to be
+                            repaired. This role exists to ensure that the site is working properly and gives users the
+                            experience we intended.</p>
                         <br>
                         <span style="font-size: 2em;">
                             <i class="fab fa-linkedin-in"></i>
@@ -182,40 +206,26 @@
                         <br>
                         <h6 class="card-subtitle mb-2 text-muted">S1824614 | MALSTO200@caledonian.ac.uk</h6>
                         <p>Project roles: <b>Project Manager</b> & <b>Software Test Analyst</b></p>
-                        <p>I have undertaken the roles of Project Manager and Software Test Analyst. during the creation of the locate-a-quake website. As a PM I am responsible for the scheduling and allocation of work between the team and to make sure locate-a-quake progresses towards a successful conclusion.</p>
+                        <p>I have undertaken the roles of Project Manager and Software Test Analyst. during the creation
+                            of the locate-a-quake website. As a PM I am responsible for the scheduling and allocation of
+                            work between the team and to make sure locate-a-quake progresses towards a successful
+                            conclusion.</p>
                         <p>
-                            As the Software Test Analyst, I am tasked with the creation of the overall test strategy in order to minimise risks and prevent problems that could cause delays to the project. Also, I will be testing the website throughout with other members of the team making sure the services it provides are streamlined and bug free.
+                            As the Software Test Analyst, I am tasked with the creation of the overall test strategy in
+                            order to minimise risks and prevent problems that could cause delays to the project. Also, I
+                            will be testing the website throughout with other members of the team making sure the
+                            services it provides are streamlined and bug free.
                         </p>
                         <br>
                         <span style="font-size: 2em;">
-                            <a href="http://linkedin.com/in/matthew-alston-79a284183" target="blank"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="http://linkedin.com/in/matthew-alston-79a284183" target="blank"><i
+                                    class="fab fa-linkedin-in"></i></a>
                             <a href="https://github.com/mattahu" target="blank"><i class="fab fa-github"></i></a>
                         </span>
                     </div>
                 </div>
 
-                <div class="collapse" id="afaqInfo">
-                    <div class="card card-body">
-                        <h2>Afaq Ahmad <img src="authorimg/afaq.png" class="author-img-profile" alt=""></h2>
-                        <br>
-                        <h6 class="card-subtitle mb-2 text-muted">S1625481 | AAHMAD204@caledonian.ac.uk</h6>
-                        <p>Project roles: <b>Software Architect</b> & <b>Process Analyst</b></p>
-                        <p>My roles in this project will be to play my part as a software architect and process analyst. As a software architect I was responsible for making the high level designs choices and dictate technical standards. I was responsible for deciding on the software pattern that would best suit Locate-A-Quake. The pattern decided was event based and MVC for the user to manipulate the view without changing the underlying data. </p>
-                        <p>As a process analyst I worked with my team members to decide on the best way to undertake the project and break down the work into smaller tasks as this will help everyone improve their understanding of the project.</p>
-                        <br>
-                    </div>
-                </div>
 
-                <div class="collapse" id="jackInfo">
-                    <div class="card card-body">
-                        <h2>Jack Moore <img src="authorimg/jack.png" class="author-img-profile" alt=""></h2>
-                        <br>
-                        <h6 class="card-subtitle mb-2 text-muted">S1628300 | JMOORE200@caledonian.ac.uk</h6>
-                        <p>Project roles: <b>SCRUM Master</b> & <b>Software Tester</b></p>
-                        <p>As a Scrum Master I was required to ensure the team followed an agile methodology when implementing new features to the site and to regularly update the next goal in the development of the locate-a-quake site. By using Scrum we aimed to regularly update the site with new functionality. As a software tester my role involved navigating the site and evaluating the user experience and looking for any errors or broken features that need to be repaired. This role exists to ensure that the site is working properly and gives users the experience we intended.</p>
-                        <br>
-                    </div>
-                </div>
             </div>
             <!-- end sidebar column content -->
         </div>
